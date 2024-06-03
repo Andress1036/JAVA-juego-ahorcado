9b519b32dfff99ddefb24223892a36a220313820
Andres Cortes <edinsoncortes1@gmail.com>
2024-06-03 17:27:49 -0500

    Mi primer juego en java el Ahorcado

---
+++ src/AhorcadoGame.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class AhorcadoGame {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Juego el Ahorcado");
+
+        String palabraSecreta = "camino";
+        int cantidadIntentos = 5;
+        int intentos = 0;
+        boolean palabraAdivinada = false;
+
+        char[] letrasAdivinadas = new char[palabraSecreta.length()];
+        for(int i = 0; i < letrasAdivinadas.length; i++){
+            letrasAdivinadas[i] = '_';
+        }
+
+        while(!palabraAdivinada && intentos < cantidadIntentos){
+
+            System.out.println("Adivina la palabra: " + String.valueOf(letrasAdivinadas) + " (" + palabraSecreta.length() + " letras)");
+
+            System.out.println("Introduce una letra: ");
+            char letra = Character.toLowerCase(sc.next().charAt(0));
+            boolean letraCorrecta = false;
+            for(int i = 0; i < palabraSecreta.length(); i++){
+
+                if(palabraSecreta.charAt(i) == letra){
+                    letrasAdivinadas[i] = letra;
+                    letraCorrecta = true;
+                }
+            }
+
+            if(!letraCorrecta){
+                intentos++;
+                System.out.println("¡Letra incorrecta! te quedan " + (cantidadIntentos - intentos) + " intentos.");
+            }
+
+            if(String.valueOf(letrasAdivinadas).equals(palabraSecreta)){
+                palabraAdivinada = true;
+                System.out.println("¡Felicidades has adivinado la palabra secreta! = '" + palabraSecreta + "'");
+            }
+
+        }
+
+        if(!palabraAdivinada){
+            System.out.println("¡Que mal, no tienes más intentos! YOU LOSE");
+        }
+
+        sc.close();
+
+
+
+    }
+}
